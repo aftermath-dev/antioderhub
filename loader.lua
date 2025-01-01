c72f581ed95e5cb2a0e21ff9cee120739b97f57c
aftermath-dev <evgenijzulin629@gmail.com>
2025-01-02 00:11:53 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,21 @@
-local StarterGui = game:GetService("StarterGui")
+-- AntiOder Loader
 
-StarterGui:SetCore("SendNotification", {
-    Title = "AntiOder Hub";
-    Text = "Starting... its take 5 sec";
-    Duration = 5;  -- Durée de la notification en secondes
-    Button1 = "OK"; -- Bouton optionnel
-})
+-- Instances:
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/main.lua"))()
+local ScreenGui = Instance.new("ScreenGui")
+local ImageLabel = Instance.new("ImageLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+
+ImageLabel.Parent = ScreenGui
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.3800000, 0, 0.100000000, 0)
+ImageLabel.Size = UDim2.new(0, 314, 0, 296)
+ImageLabel.Image = "rbxassetid://135589479904919"
+wait(4)
+ImageLabel:Destroy()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/loader1.lua"))()
