50b4f847f85585189719276837c819cb6f86301e
aftermath-dev <evgenijzulin629@gmail.com>
2025-01-13 15:19:39 +0700

    Update loader3.lua

--- loader3.lua
+++ loader3.lua
@@ -2,3 +2,4 @@ if game.PlaceId == 4924922222 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/brookhaven"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/main"))()
+end
