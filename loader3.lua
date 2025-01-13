be7833028b1bd580509ede86eff1dec49be40b77
aftermath-dev <evgenijzulin629@gmail.com>
2025-01-13 15:15:02 +0700

    Update loader3.lua

--- loader3.lua
+++ loader3.lua
@@ -1,13 +1,23 @@
 if game.PlaceId == 4924922222 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/brookhaven"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/main"))()
 if game.PlaceId == 142823291 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/mm2"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/main"))()
 if game.PlaceId == 2413927524 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/trr"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/main"))()
 if game.PlaceId == 6654918151 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/tw"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/main"))()
 if game.PlaceId == 893973440 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/ff"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/main"))()
 if game.PlaceId == 189707 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/nd"))()
 else
