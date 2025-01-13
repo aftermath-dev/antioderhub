cd2527ffb7a1bd27846d751114c1282c35b94c74
aftermath-dev <evgenijzulin629@gmail.com>
2025-01-13 14:53:15 +0700

    Update loader3.lua

--- loader3.lua
+++ loader3.lua
@@ -6,5 +6,7 @@ if game.PlaceId == 2413927524 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/trr"))()
 if game.PlaceId == 6654918151 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/tw"))()
+if game.PlaceId == 893973440 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/ff"))()
                 
     
