35cd61a44ab6f42a4f53417810a5c6be85e82e92
aftermath-dev <evgenijzulin629@gmail.com>
2025-02-02 00:09:51 +0700

    Update NewScript.lua

--- NewScript.lua
+++ NewScript.lua
@@ -19,7 +19,7 @@ local Window = Fluent:CreateWindow({
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 340),
     Acrylic = false,
-    Theme = "Dark",
+    Theme = "Aqua",
     MinimizeKey = Enum.KeyCode.LeftControl
 })
 
