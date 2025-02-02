1ee4a78c3ff593b2e0db1a48b8f53e99842419f2
aftermath-dev <evgenijzulin629@gmail.com>
2025-02-02 15:16:15 +0700

    Update and rename loader3.lua to Games.lua

---
+++ Games.lua
@@ -0,0 +1,15 @@
+if game.PlaceId == 4924922222 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/brookhaven"))()
+elseif game.PlaceId == 142823291 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/mm2"))()
+elseif game.PlaceId == 2413927524 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/trr"))()
+elseif game.PlaceId == 6654918151 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/tw"))()
+elseif game.PlaceId == 893973440 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/ff"))()
+elseif game.PlaceId == 189707 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/nd"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/antioderhub/refs/heads/main/main"))()
+end
